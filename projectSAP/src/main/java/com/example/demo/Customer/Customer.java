fff2ccadf797cb08084e468a1f070d123a0e4c24
Stanislav Mitev <mitev690@gmail.com>
2024-04-13 15:16:03 +0300

    Update Customer class

--- projectSAP/src/main/java/com/example/demo/Customer/Customer.java
+++ projectSAP/src/main/java/com/example/demo/Customer/Customer.java
@@ -4,13 +4,71 @@ import com.example.demo.User;
 import com.example.demo.UserType;
 
 public class Customer extends User {
-    public Customer(Long id, String password, String username) {
-        super(id, password, username);
+    private Long customerId;
+    private Integer loyaltyPoints;
+
+    public Customer() {
+
+    }
+
+    public Customer(Long customerId,
+                    Integer loyaltyPoints) {
+        this.customerId = customerId;
+        this.loyaltyPoints = loyaltyPoints;
+    }
+
+    public Customer(String password,
+                    String username,
+                    Long customerId,
+                    Integer loyaltyPoints) {
+        super(password, username);
+        this.customerId = customerId;
+        this.loyaltyPoints = loyaltyPoints;
+    }
+
+    public Customer(Long userId,
+                    String firstName,
+                    String lastName,
+                    String password,
+                    String username,
+                    String email,
+                    Long customerId,
+                    Integer loyaltyPoints) {
+        super(userId, firstName, lastName, password, username, email);
+        this.customerId = customerId;
+        this.loyaltyPoints = loyaltyPoints;
+    }
+
+    public Customer(String firstName,
+                    String lastName,
+                    String password,
+                    String username,
+                    String email,
+                    Long customerId,
+                    Integer loyaltyPoints) {
+        super(firstName, lastName, password, username, email);
+        this.customerId = customerId;
+        this.loyaltyPoints = loyaltyPoints;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Integer getLoyaltyPoints() {
+        return loyaltyPoints;
+    }
+
+    public void setLoyaltyPoints(Integer loyaltyPoints) {
+        this.loyaltyPoints = loyaltyPoints;
     }
 
     @Override
     public UserType getUserType() {
         return UserType.CUSTOMER;
-
     }
 }
