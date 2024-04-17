7cf8dba7544253106d97884f83b3c70b42ac7d78
MartinYovchev <144015990+MartinYovchev@users.noreply.github.com>
2024-04-17 18:24:27 +0300

    Add major Product functionalities

--- projectSAP/src/main/java/com/example/demo/products/ProductRepository.java
+++ projectSAP/src/main/java/com/example/demo/products/ProductRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository
-        extends JpaRepository<Products, Long> {
+        extends JpaRepository<Products, Integer> {
 }
--- projectSAP/src/main/java/com/example/demo/products/ProductService.java
+++ projectSAP/src/main/java/com/example/demo/products/ProductService.java
@@ -1,13 +1,29 @@
 package com.example.demo.products;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
 
     @Autowired
-    private ProductRepository productRepository;  // Assuming a ProductRepository is injected
+    private ProductRepository productRepository;
 
+    public Products saveProduct(Products product) {
+        return productRepository.save(product);
+    }
 
+    public Optional<Products> getProductById(Integer id) {
+        return productRepository.findById(id);
+    }
+
+    public List<Products> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public void deleteProductById(Integer id) {
+        productRepository.deleteById(id);
+    }
 }
