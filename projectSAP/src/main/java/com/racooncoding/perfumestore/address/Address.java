7c8178240b806e585b74d8b2556307d3eb69cf0d
Stanislav Mitev <mitev690@gmail.com>
2024-04-26 19:06:06 +0300

    Add equal() and hashCode()

--- projectSAP/src/main/java/com/racooncoding/perfumestore/address/Address.java
+++ projectSAP/src/main/java/com/racooncoding/perfumestore/address/Address.java
@@ -2,6 +2,8 @@ package com.racooncoding.perfumestore.address;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table
 public class Address {
@@ -87,5 +89,35 @@ public class Address {
 
     public void setAddressLine2(String addressLine2) {
         this.addressLine2 = addressLine2;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "addressId=" + addressId +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", postalCode=" + postalCode +
+                ", addressLine1='" + addressLine1 + '\'' +
+                ", addressLine2='" + addressLine2 + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(addressId, address.addressId) &&
+                Objects.equals(city, address.city) &&
+                Objects.equals(state, address.state) &&
+                Objects.equals(postalCode, address.postalCode) &&
+                Objects.equals(addressLine1, address.addressLine1) &&
+                Objects.equals(addressLine2, address.addressLine2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(addressId, city, state, postalCode, addressLine1, addressLine2);
     }
 }
