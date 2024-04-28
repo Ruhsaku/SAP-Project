1397516aa3cc3562263a4736184827c276b8e751
Stanislav Mitev <mitev690@gmail.com>
2024-04-28 13:30:13 +0300

    Add TODO for products checkout

--- projectSAP/src/main/java/com/racooncoding/perfumestore/cart/Cart.java
+++ projectSAP/src/main/java/com/racooncoding/perfumestore/cart/Cart.java
@@ -1,21 +1,17 @@
-//package com.example.demo.cart;
-//
-//import com.example.demo.ordersproducts.OrdersProducts;
-//import jakarta.persistence.*;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//
-////@Entity
-////@Table
-//public class Cart {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-////    @OneToMany(mappedBy = "cart")
-//    private List<OrdersProducts> cartItems = new ArrayList<>();
-//    // Other attributes and getters/setters
-//}
-//
+package com.racooncoding.perfumestore.cart;
+
+import com.racooncoding.perfumestore.ordersproducts.OrdersProducts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private Long id;
+
+//    @OneToMany(mappedBy = "cart")
+    private List<OrdersProducts> cartItems = new ArrayList<>();
+    // Other attributes and getters/setters
+
+    // TODO --> Cart class for checkout
+}
+
