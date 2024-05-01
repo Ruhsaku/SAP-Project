ef7383158d0de0e13f9d65afae3fde1ce192cc29
Stanislav Mitev <mitev690@gmail.com>
2024-05-01 14:31:38 +0300

    Remove unnecessary comments

--- projectSAP/src/main/java/com/racooncoding/perfumestore/web/WebPageController.java
+++ projectSAP/src/main/java/com/racooncoding/perfumestore/web/WebPageController.java
@@ -1,9 +1,6 @@
 package com.racooncoding.perfumestore.web;
 
-import com.racooncoding.perfumestore.checkout.ChargeRequest;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
